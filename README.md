9fc712f744dd3d9ff3286d4115b516f5b60ed547
DavidBruchmann <david.bruchmann@gmail.com>
2018-10-19 05:35:29 +0700

    Feature: TYPO3 6.1

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Backend Simplifier (tsconf)
 ===========================
 The Backend Simplifier offers the configuration of the eight most commonly used
-porperties for simplifying the TYPO3 backend by a mouse click. User interface
+properties for simplifying the TYPO3 backend by a mouse click. User interface
 is the extension manager. Enable page tree uids, use indvidual page tree icons,
 activate the extended view and the clipboard and the admin panel for all editors.
 Extend the cache thunderbolts with one for the system cache.
--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "tsconf".
 #
-# Auto generated 19-08-2015 00:11
+# Auto generated 19-08-2015 17:34
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,12 +15,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Configure the TSconfig for your pages and users with the mouse! tsconf offers the eight most commonly used porperties like page tree uids, activated extended view, activated clipboard, activated admin panel and cache management for editors among others.',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '1.1.1',
+	'version' => '1.2.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '',
+	'TYPO3_version' => '4.5.0-6.1.99',
 	'PHP_version' => '',
 	'module' => '',
 	'state' => 'stable',
