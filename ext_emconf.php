ec5871f238ccaed8bda1690a32bde93a68ef9e63
DavidBruchmann <david.bruchmann@gmail.com>
2018-10-19 04:47:58 +0700

    SEO: Extension title is moved from tsConf to tsConfig

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "tsconf".
 #
-# Auto generated 11-08-2015 20:44
+# Auto generated 08-08-2015 11:33
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -11,11 +11,11 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'TSconf by extManager',
-	'description' => 'TS configuration for pages and users by the extension manager',
+	'title' => 'TSconfig Pages and Users by extManager',
+	'description' => 'Configure the TSconfig for your pages and users with the mouse! tsconf offers the eight most commonly used porperties like page tree uids, activated extended view, activated clipboard, activated admin panel and cache management for editors among others.',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '0.1.0',
+	'version' => '0.1.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
