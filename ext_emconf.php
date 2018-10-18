41bccb85081a13b87a3823e7ddeaa316443012ff
DavidBruchmann <david.bruchmann@gmail.com>
2018-10-19 05:11:36 +0700

    IMPROVEMENT: Downgrade for TYPO3 4.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "tsconf".
 #
-# Auto generated 11-08-2015 03:51
+# Auto generated 09-08-2015 09:21
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,12 +15,12 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Configure the TSconfig for your pages and users with the mouse! tsconf offers the eight most commonly used porperties like page tree uids, activated extended view, activated clipboard, activated admin panel and cache management for editors among others.',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '1.0.1',
+	'version' => '1.1.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
-	'TYPO3_version' => '4.5.0-0.0.0',
+	'TYPO3_version' => '',
 	'PHP_version' => '',
 	'module' => '',
 	'state' => 'stable',
