bb0390e0e1cc14d0d32054cf4cd1fe78618983d5
DavidBruchmann <david.bruchmann@gmail.com>
2018-10-19 01:40:25 +0700

    first commit

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "tsconf".
+#
+# Auto generated 11-08-2015 08:53
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TSconf by extManager',
+	'description' => 'TS configuration for pages and users by the extension manager',
+	'category' => 'be',
+	'shy' => 0,
+	'version' => '0.0.4',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'TYPO3_version' => '',
+	'PHP_version' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Dirk Wildt - Die Netzmacher GbR',
+	'author_email' => 'http://wildt.at.die-netzmacher.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+);
+
+?>
